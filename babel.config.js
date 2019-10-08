1a6cb0a358b9908d56ff09302d69aa472b944e97
Zachary Hueras <zhueras@amdtelemedicine.com>
2019-10-07 23:59:05 -0400

    Fix babel config

--- babel.config.js
+++ babel.config.js
@@ -2,8 +2,7 @@ module.exports = function(api) {
     api.cache(true);
     return {
         'presets': [['@babel/preset-env', {'targets': {'browsers': ['last 2 Chrome versions']}}]],
-        'plugins':
-            ['@babel/plugin-proposal-object-rest-spread', '@babel/plugin-proposal-class-properties']
+        'plugins': ['@babel/plugin-proposal-object-rest-spread']
     };
 }
 
