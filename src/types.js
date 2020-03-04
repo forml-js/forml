fd064b74b5152157c550c3c5ca43c83e92caa50b
Zachary Hueras <zhueras@amdtelemedicine.com>
2020-03-03 19:51:22 -0500

    Allow numeric indexes in keys

--- src/types.js
+++ src/types.js
@@ -1,7 +1,7 @@
 import t from 'prop-types';
 
 export const FormType  = t.shape({
-    key: t.arrayOf(t.string),
+    key: t.arrayOf(t.oneOfType([t.string, t.number])),
     type: t.string,
 });
 export const FormsType = t.arrayOf(t.oneOfType([
