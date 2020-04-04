5524bc16efa60c87e98cabe4e45f2514ef7d2bd0
Zachary Hueras <zhueras@amdtelemedicine.com>
2020-04-04 00:30:39 -0400

    Even smarter label shrink logic

--- src/components/decorator/mui/label.js
+++ src/components/decorator/mui/label.js
@@ -5,6 +5,6 @@ import {createElement as h} from 'react';
  * @component
  */
 export default function Label(props) {
-    const shrink = props.focused || (props.value !== undefined);
+    const shrink = props.focused || (props.value !== undefined && props.value !== '');
     return h(InputLabel, {shrink, ...props});
 }
