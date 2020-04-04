62e7e37269188b1fcc3a5812e0154ec67f2094f3
Zachary Hueras <zhueras@amdtelemedicine.com>
2020-04-03 21:53:57 -0400

    Include some spacing in mui fieldset

--- src/components/decorator/mui/fieldset.js
+++ src/components/decorator/mui/fieldset.js
@@ -2,15 +2,24 @@ import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import FormControl from '@material-ui/core/FormControl';
 import FormLabel from '@material-ui/core/FormLabel';
+import {makeStyles} from '@material-ui/core/styles';
 import {createElement as h} from 'react';
 
 /**
  * @component
  */
+const useStyles = makeStyles(function(theme) {
+    return {
+        root: {margin: theme.spacing(0, 0, 0, 1)},
+    };
+});
 export default function FieldSet(props) {
     const {title} = props;
-    return h(Card, {}, h(CardContent, {}, h(FormControl, {fullWidth: true, component: 'fieldset'}, [
-                             h(FormLabel, {key: 'label', component: 'legend'}, title),
-                             props.children,
-                         ])));
+    const classes = useStyles();
+    return h(Card,
+             {className: classes.root},
+             h(CardContent, {}, h(FormControl, {fullWidth: true, component: 'fieldset'}, [
+                   h(FormLabel, {key: 'label', component: 'legend'}, title),
+                   props.children,
+               ])));
 }
