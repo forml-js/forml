876660d8aa2da8993bd829dd96a2ece33534c73e
Zachary Hueras <zhueras@amdtelemedicine.com>
2020-10-27 19:34:30 -0400

    Additional tests

---
+++ test/components/schema-field.test.js
@@ -0,0 +1,91 @@
+import Context, { useMapper } from '../../src/context';
+import { SchemaField } from '../../src/components/schema-field';
+import { createElement as h } from 'react';
+import {defaultMapper, getMapper} from '../../src/components/mapper';
+import {getDecorator} from '../../src/components/decorator';
+import {getLocalizer} from '../../src/localizer';
+import * as util from '../../src/util';
+import renderer from 'react-test-renderer';
+
+jest.mock('../../src/components/mapper', function(...args) {
+    const mapper = new Proxy({}, {
+        get(target, prop, receiver) {
+            if (prop in target) {
+                return target[prop];  
+            }
+        },
+        set(target, prop, value) {
+            if (prop in target) {
+                target[prop].mockImplementation(value);
+            } else {
+                target[prop] = jest.fn(value);
+            }
+            return target[prop];
+        }
+    });
+    return {
+        defaultMapper() {
+            return mapper();
+        },
+        getMapper(additional = {}) {
+            for (let key in additional) {
+                mapper[key] = additional[key];
+            }
+            return mapper;
+        }
+    }
+});
+
+function getContext(schema, model = '', errors = {}) {
+    const decorator = getDecorator({});
+    const localizer = getLocalizer({});
+    const getValue = jest.fn(util.valueGetter(model, schema));
+    const setValue = jest.fn(util.valueSetter(model, schema));
+    const getError = jest.fn(util.errorGetter(errors, schema));
+    const mapper = getMapper({
+        array: jest.fn(() => 'array'),
+        checkbox: jest.fn(() => 'checkbox'),
+        date: jest.fn(() => 'date'),
+        datetime: jest.fn(() => 'datetime'),
+        dynamic: jest.fn(() => 'dynamic'),
+        fieldset: jest.fn(() => 'fieldset'),
+        help: jest.fn(() => 'help'),
+        integer: jest.fn(() => 'integer'),
+        multiselect: jest.fn(() => 'multiselect'),
+        null: jest.fn(() => 'null'),
+        number: jest.fn(() => 'number'),
+        password: jest.fn(() => 'password'),
+        select: jest.fn(() => 'select'),
+        tabs: jest.fn(() => 'tabs'),
+        text: jest.fn(() => 'text'),
+        textarea: jest.fn(() => 'textarea'),
+        tuple: jest.fn(() => 'tuple'),
+        file: jest.fn(() => 'file'),
+    });
+    return {mapper, decorator, localizer, getValue, setValue, getError};
+
+}
+
+test('does not render if no mapped Field is found for type', function() {
+    const schema = {type: 'object'};
+    const form = {key: [], type: 'custom', schema}
+    const context = getContext(schema, {});
+
+    const component = renderer.create(h(Context.Provider, {value: context}, h(SchemaField, {form, schema})));
+    expect(component).toMatchSnapshot();
+});
+
+test('uses mapper from context', function() {
+    const schema = {type: 'string'};
+    const form = {key: [], type: 'text', schema};
+    const context = getContext(schema);
+
+    const component = renderer.create(h(Context.Provider, {
+        value: context
+    }, h(SchemaField, {
+        form , schema
+    })));
+
+    expect(context.mapper.text).toHaveBeenCalled();
+});
+
--- test/components/schema-form.test.js
+++ test/components/schema-form.test.js
@@ -2,50 +2,48 @@ import { SchemaForm } from '../../src/components/schema-form';
 import { createElement as h } from 'react';
 import renderer from 'react-test-renderer';
 
-describe('SchemaForm', function() {
-    test('uses the supplied mapper', function() {
-        const mapper = {
-            text: jest.fn((props) => h('div'))
-        };
+test('uses the supplied mapper', function() {
+    const mapper = {
+        text: jest.fn((props) => h('div'))
+    };
 
-        const component = renderer.create(h(SchemaForm, {
-            schema: { type: 'string' },
-            form: ['*'],
-            mapper
-        }));
+    const component = renderer.create(h(SchemaForm, {
+        schema: { type: 'string' },
+        form: ['*'],
+        mapper
+    }));
 
-        expect(mapper.text).toHaveBeenCalled();
-    })
+    expect(mapper.text).toHaveBeenCalled();
+})
 
-    test('uses the supplied localizer', function() {
-        const localizer = {
-            getLocalizedString: jest.fn((string) => {
-                return string;
-            }),
-        };
+test('uses the supplied localizer', function() {
+    const localizer = {
+        getLocalizedString: jest.fn((string) => {
+            return string;
+        }),
+    };
 
-        const component = renderer.create(h(SchemaForm, {
-            schema: { type: 'string', title: 'test' },
-            form: ['*'],
-            localizer,
-        }))
+    const component = renderer.create(h(SchemaForm, {
+        schema: { type: 'string', title: 'test' },
+        form: ['*'],
+        localizer,
+    }))
 
-        expect(localizer.getLocalizedString).toHaveBeenCalledWith('test');
-    })
+    expect(localizer.getLocalizedString).toHaveBeenCalledWith('test');
+})
 
-    test('uses the supplied decorator', function() {
-        const decorator = {
-            Input: {
-                Form: jest.fn((_props) => h('div')),
-            }
+test('uses the supplied decorator', function() {
+    const decorator = {
+        Input: {
+            Form: jest.fn((_props) => h('div')),
         }
+    }
 
-        renderer.create(h(SchemaForm, {
-            schema: { type: 'string' },
-            form: [{ key: [], type: 'text' }],
-            decorator,
-        }));
+    renderer.create(h(SchemaForm, {
+        schema: { type: 'string' },
+        form: [{ key: [], type: 'text' }],
+        decorator,
+    }));
 
-        expect(decorator.Input.Form).toHaveBeenCalled();
-    })
-});
+    expect(decorator.Input.Form).toHaveBeenCalled();
+})
