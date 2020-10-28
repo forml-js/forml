52777bf1731e8bebb65b6e12ea452e0b46f4c06d
Zachary Hueras <zhueras@amdtelemedicine.com>
2020-10-28 02:20:43 -0400

    More tests and snapshots

---
+++ test/data.test.js
@@ -0,0 +1,66 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import * as renderer from 'react-test-renderer';
+import { createElement as h } from 'react';
+import { SchemaForm, decorators } from '../lib';
+import * as util from '../lib/util';
+
+function importAll(directory) {
+    const files = fs.readdirSync(directory, { withFileTypes: true });
+    const result = {};
+
+    for (let file of files) {
+        if (file.isDirectory()) continue;
+        const filepath = path.resolve(directory, file.name);
+
+        if (file.name.endsWith('.json')) {
+            result[file.name] = JSON.parse(fs.readFileSync(filepath, { encoding: 'utf8' }));
+        } else if (file.name.endsWith('.js')) {
+            result[file.name] = require(filepath);
+        }
+
+        if (result[file.name].default)
+            result[file.name] = result[file.name].default;
+
+        if (!result[file.name].schema)
+            Reflect.deleteProperty(result, file.name);
+    }
+
+    return result;
+}
+
+describe('samples', function() {
+    const samplesPath = path.resolve(__dirname, '../example/data');
+    const samples = importAll(samplesPath);
+    for (let file in samples) {
+        const filename = path.basename(file, path.extname(file));
+        describe(filename, function() {
+            test('renders with a defaultForSchema model', async function() {
+                let { schema, form } = await samples[file];
+                let model = util.defaultForSchema(schema);
+
+                const component = renderer.create(h(SchemaForm, {
+                    schema,
+                    form,
+                    model,
+                })).toJSON();
+
+                expect(component).toMatchSnapshot();
+            });
+
+            test('renders with the MaterialUI decorator', async function() {
+                let { schema, form } = await samples[file];
+                let model = util.defaultForSchema(schema);
+
+                const component = renderer.create(h(SchemaForm, {
+                    schema,
+                    form,
+                    model,
+                    decorator: decorators.mui,
+                })).toJSON();
+
+                expect(component).toMatchSnapshot();
+            })
+        });
+    }
+});
