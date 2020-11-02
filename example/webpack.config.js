be1887e67dd89c54fe2a69ed4f06700ff147de6d
Zachary Hueras <zhueras@amdtelemedicine.com>
2020-11-02 18:53:00 -0500

    Fix bundle paths

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -1,11 +1,11 @@
-const path     = require('path');
+const path = require('path');
 module.exports = {
     mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
     entry: path.resolve('./index.js'),
     output: {
         filename: 'bundle.js',
-        publicPath: '/',
-        path: path.resolve('./dist'),
+        publicPath: '/rjsf/dist/',
+        path: path.resolve('../dist'),
     },
     devtool: 'eval-source-map',
     resolve: {
@@ -13,17 +13,21 @@ module.exports = {
             rjsf: path.resolve(__dirname, '../src'),
             react: path.resolve('./node_modules/react'),
             'react-dom': path.resolve('./node_modules/react-dom'),
-            '@material-ui/core': path.resolve('./node_modules/@material-ui/core'),
-            '@material-ui/pickers': path.resolve('./node_modules/@material-ui/pickers'),
-        }
+            '@material-ui/core': path.resolve(
+                './node_modules/@material-ui/core'
+            ),
+            '@material-ui/pickers': path.resolve(
+                './node_modules/@material-ui/pickers'
+            ),
+        },
     },
     module: {
         rules: [
             {
                 test: /\.m?jsx?$/,
                 exclude: /node_modules/,
-                use: {loader: 'babel-loader'},
+                use: { loader: 'babel-loader' },
             },
-        ]
-    }
+        ],
+    },
 };
