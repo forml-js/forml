278432e63fe14d783ff51a1d29ce2f18ef525627
Zachary Hueras <zhueras@amdtelemedicine.com>
2020-11-02 18:41:02 -0500

    Update example

--- example/index.js
+++ example/index.js
@@ -1,16 +1,23 @@
 import MomentUtils from '@date-io/moment';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
-import {makeStyles} from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
-import {MuiPickersUtilsProvider} from '@material-ui/pickers';
+import { MuiPickersUtilsProvider } from '@material-ui/pickers';
 import debug from 'debug';
 import moment from 'moment';
 import Prism from 'prismjs';
-import {Component, createElement as h, Fragment, useEffect, useMemo, useState} from 'react';
-import {render} from 'react-dom';
+import {
+    Component,
+    createElement as h,
+    Fragment,
+    useEffect,
+    useMemo,
+    useState,
+} from 'react';
+import { render } from 'react-dom';
 import SimpleEditor from 'react-simple-code-editor';
-import {decorators, SchemaForm, util} from 'rjsf';
+import { decorators, SchemaForm, util } from 'rjsf';
 
 const log = debug('rjsf:example');
 
@@ -22,63 +29,71 @@ async function loadPrism() {
     await import('prismjs/components/prism-javascript');
 }
 
-
 function useEditable(defaultValue) {
-    const [value, doSetValue] = useState({value: defaultValue});
-    const [json, doSetJSON]   = useState({value: JSON.stringify(defaultValue, undefined, 2)});
+    const [value, doSetValue] = useState({ value: defaultValue });
+    const [json, doSetJSON] = useState({
+        value: JSON.stringify(defaultValue, undefined, 2),
+    });
 
     function setValue(value) {
-        doSetValue({value});
-        doSetJSON({value: JSON.stringify(value, undefined, 2)});
+        doSetValue({ value });
+        doSetJSON({ value: JSON.stringify(value, undefined, 2) });
     }
 
     function setJSON(json) {
-        doSetJSON({value: json});
+        doSetJSON({ value: json });
         try {
-            doSetValue({value: JSON.parse(json)});
+            doSetValue({ value: JSON.parse(json) });
         } catch (err) {
             /** noop */
         }
     }
 
-    return {value: value.value, json: json.value, setValue, setJSON};
+    return { value: value.value, json: json.value, setValue, setJSON };
 }
 
 function Editor(props) {
-    if (!props.value)
-        return null;
+    if (!props.value) return null;
 
     return h(SimpleEditor, {
         value: props.value,
         highlight: (code) => {
-            return Prism.highlight(code, Prism.languages.javascript, 'javascript')
+            return Prism.highlight(
+                code,
+                Prism.languages.javascript,
+                'javascript'
+            );
         },
         padding: 10,
-        style: {fontFamily: 'Hack, monospace', fontSize: 12},
+        style: { fontFamily: 'Hack, monospace', fontSize: 12 },
         onValueChange,
     });
 
     function onValueChange(value) {
-        props.onChange({target: {value}}, value);
+        props.onChange({ target: { value } }, value);
     }
 }
 
 class ErrorBoundary extends Component {
     constructor(props) {
         super(props);
-        this.state = {hasError: false};
+        this.state = { hasError: false };
     }
 
     componentDidCatch(error, info) {
-        this.setState({hasError: true, error, info});
+        this.setState({ hasError: true, error, info });
     }
 
     render() {
         if (this.state.hasError) {
             return h(Fragment, {}, [
-                h(Typography, {variant: 'h6'}, 'Something went wrong'),
-                h(Typography, {variant: 'body1'}, this.state.error.stack),
-                h(Typography, {variant: 'caption'}, JSON.stringify(this.state.info)),
+                h(Typography, { variant: 'h6' }, 'Something went wrong'),
+                h(Typography, { variant: 'body1' }, this.state.error.stack),
+                h(
+                    Typography,
+                    { variant: 'caption' },
+                    JSON.stringify(this.state.info)
+                ),
             ]);
         }
 
@@ -87,17 +102,15 @@ class ErrorBoundary extends Component {
 }
 
 function importAll(context) {
-    const keys   = context.keys();
+    const keys = context.keys();
     const result = {};
 
     for (let key of keys) {
         result[key] = context(key);
 
-        if (result[key].default)
-            result[key] = result[key].default;
+        if (result[key].default) result[key] = result[key].default;
 
-        if (!result[key].schema)
-            Reflect.deleteProperty(result, key);
+        if (!result[key].schema) Reflect.deleteProperty(result, key);
     }
 
     return result;
@@ -106,15 +119,22 @@ function importAll(context) {
 const samples = importAll(require.context('./data/', true, /\.js(on)?$/));
 
 function SelectExample(props) {
-    const enm    = Object.keys(samples);
-    const titles = enm.map(k => samples[k].schema.title);
+    const enm = Object.keys(samples);
+    const titles = enm.map((k) => samples[k].schema.title);
 
-    const form              = [{key: [], title: 'Sample', titles}];
-    const schema            = {type: 'string', enum: enm};
-    const model             = props.selected;
-    const decorator         = decorators.mui;
+    const form = [{ key: [], title: 'Sample', titles }];
+    const schema = { type: 'string', enum: enm };
+    const model = props.selected;
+    const decorator = decorators.mui;
 
-    return h(SchemaForm, {schema, form, model, onChange, decorator, onModelChange});
+    return h(SchemaForm, {
+        schema,
+        form,
+        model,
+        onChange,
+        decorator,
+        onModelChange,
+    });
 
     function onModelChange(model) {
         log('onModelChange() : %o', model);
@@ -129,11 +149,11 @@ function SelectExample(props) {
     }
 }
 function RenderExample(props) {
-    const {schema, form, model} = props;
-    const {onChange}            = props;
-    const {localizer}           = props;
-    const {mapper}              = props;
-    const decorator             = decorators.mui;
+    const { schema, form, model } = props;
+    const { onChange } = props;
+    const { localizer } = props;
+    const { mapper } = props;
+    const decorator = decorators.mui;
 
     const formProps = {
         schema,
@@ -149,31 +169,30 @@ function RenderExample(props) {
 }
 
 function getSample(selected) {
-    if (selected in samples)
-        return samples[selected];
+    if (selected in samples) return samples[selected];
 
-    return {schema: {type: 'null'}, form: ['*']};
+    return { schema: { type: 'null' }, form: ['*'] };
 }
 
-const useStyles = makeStyles(function() {
+const useStyles = makeStyles(function () {
     return {
-        root: {display: 'flex', flexDirection: 'row'},
-        manager: {flex: '0 0 300px'},
-        example: {flex: '1 0 600px'},
+        root: { display: 'flex', flexDirection: 'row' },
+        manager: { flex: '0 0 300px' },
+        example: { flex: '1 0 600px' },
     };
 });
 
 function Page() {
-    const classes                   = useStyles();
-    const [selected, setSelected]   = useState('');
+    const classes = useStyles();
+    const [selected, setSelected] = useState('');
     const [localizer, setLocalizer] = useState(undefined);
-    const [mapper, setMapper]       = useState(undefined);
-    const schema                    = useEditable({type: 'null'});
-    const form                      = useEditable(['*']);
-    const defaultModel              = useMemo(() => {
+    const [mapper, setMapper] = useState(undefined);
+    const schema = useEditable({ type: 'null' });
+    const form = useEditable(['*']);
+    const defaultModel = useMemo(() => {
         return util.defaultForSchema(schema.value);
     }, [schema.value]);
-    const model                     = useEditable(defaultModel);
+    const model = useEditable(defaultModel);
 
     function onModelChange(event, ...args) {
         model.setValue(args[0]);
@@ -182,45 +201,49 @@ function Page() {
     log('model : %o', model.value);
     log('rendering');
 
-    return h('div', {className: classes.root}, [
-        h(Card,
-            {className: classes.example, key: 'primary-viewport'},
-            [
-                h(CardContent,
-                    {key: 'example'},
-                    h(RenderExample, {
-                        key: `render-${schema.json}${form.json}`,
-                        schema: schema.value,
-                        form: form.value,
-                        model: model.value,
-                        onChange: onModelChange,
-                        mapper,
-                        localizer,
-                    })),
-                h(CardContent,
-                    {key: 'model'},
-                    [
-                        h(Typography, {key: 'title', variant: 'h6'}, 'Model'),
-                        h('pre', {key: 'body'}, model.json),
-                    ])
+    return h('div', { className: classes.root }, [
+        h(Card, { className: classes.example, key: 'primary-viewport' }, [
+            h(
+                CardContent,
+                { key: 'example' },
+                h(RenderExample, {
+                    key: `render-${schema.json}${form.json}`,
+                    schema: schema.value,
+                    form: form.value,
+                    model: model.value,
+                    onChange: onModelChange,
+                    mapper,
+                    localizer,
+                })
+            ),
+            h(CardContent, { key: 'model' }, [
+                h(Typography, { key: 'title', variant: 'h6' }, 'Model'),
+                h('pre', { key: 'body' }, model.json),
             ]),
-        h(Card,
-            {className: classes.manager, key: 'secondary-viewport'},
-            [
-                h(CardContent, {key: 'select-example'}, h(SelectExample, {key: 'select', selected, onChange})),
-                h(CardContent,
-                    {key: 'schema'},
-                    [
-                        h(Typography, {key: 'title', variant: 'h6'}, 'Schema'),
-                        h(Editor, {key: 'editor', value: schema.json, onChange: (e) => schema.setJSON(e.target.value)}),
-                    ]),
-                h(CardContent,
-                    {key: 'form'},
-                    [
-                        h(Typography, {key: 'title', variant: 'h6'}, 'Form'),
-                        h(Editor, {key: 'editor', value: form.json, onChange: (e) => form.setJSON(e.target.value)}),
-                    ]),
+        ]),
+        h(Card, { className: classes.manager, key: 'secondary-viewport' }, [
+            h(
+                CardContent,
+                { key: 'select-example' },
+                h(SelectExample, { key: 'select', selected, onChange })
+            ),
+            h(CardContent, { key: 'schema' }, [
+                h(Typography, { key: 'title', variant: 'h6' }, 'Schema'),
+                h(Editor, {
+                    key: 'editor',
+                    value: schema.json,
+                    onChange: (e) => schema.setJSON(e.target.value),
+                }),
             ]),
+            h(CardContent, { key: 'form' }, [
+                h(Typography, { key: 'title', variant: 'h6' }, 'Form'),
+                h(Editor, {
+                    key: 'editor',
+                    value: form.json,
+                    onChange: (e) => form.setJSON(e.target.value),
+                }),
+            ]),
+        ]),
     ]);
 
     function onChange(event, example) {
@@ -238,8 +261,10 @@ function Page() {
 async function init() {
     await loadPrism();
 
-    render(h(MuiPickersUtilsProvider, {utils: MomentUtils}, h(Page)),
-           document.getElementById('app'));
+    render(
+        h(MuiPickersUtilsProvider, { utils: MomentUtils }, h(Page)),
+        document.getElementById('app')
+    );
 }
 
 document.addEventListener('DOMContentLoaded', init);
