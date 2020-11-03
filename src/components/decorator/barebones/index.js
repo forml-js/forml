d0ab08f6c78d06dbb2cc6a445791876ff6d414ae
Zachary Hueras <zhueras@amdtelemedicine.com>
2020-11-02 19:31:16 -0500

    Remove preview components

--- src/components/decorator/barebones/index.js
+++ src/components/decorator/barebones/index.js
@@ -6,6 +6,5 @@ import * as Input from './input';
 import Label from './label';
 import * as Tabs from './tabs';
 import Text from './text';
-import * as Preview from './preview';
 
-export { Arrays, Checkbox, FieldSet, Group, Input, Label, Preview, Tabs, Text };
+export { Arrays, Checkbox, FieldSet, Group, Input, Label, Tabs, Text };
--- src/components/decorator/mui/index.js
+++ src/components/decorator/mui/index.js
@@ -6,6 +6,5 @@ import * as Input from './input';
 import Label from './label';
 import * as Tabs from './tabs';
 import Text from './text';
-import Preview from './preview';
 
-export { Arrays, Checkbox, FieldSet, Group, Input, Label, Tabs, Text, Preview };
+export { Arrays, Checkbox, FieldSet, Group, Input, Label, Tabs, Text };
