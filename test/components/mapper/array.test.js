cb156db9fbee88755f3436fcafd6a7ed1f71e616
Zachary Hueras <zhueras@amdtelemedicine.com>
2020-11-03 06:50:58 -0500

    Additional array tests

--- test/components/mapper/array.test.js
+++ test/components/mapper/array.test.js
@@ -2,8 +2,48 @@ import { render, fireEvent, waitFor } from '@testing-library/react';
 import { ArrayComponent } from '../../../src/components/mapper/array';
 import '@testing-library/jest-dom/extend-expect';
 
-import { SchemaForm, util } from '../../../src';
+import { SchemaForm, util, constants } from '../../../src';
+import { mover, creator } from '../../../src/components/mapper/array';
 import { createElement as h } from 'react';
+
+describe('item mover', function () {
+    let form;
+    let create;
+    let move;
+    let items;
+    let value;
+
+    beforeEach(function () {
+        form = {
+            type: 'array',
+            items: [{ type: 'number', key: [constants.ARRAY_PLACEHOLDER] }],
+            schema: { type: 'array', items: { type: 'number' } },
+        };
+        items = ['1', '2', '3', '4'];
+        value = [1, 2, 3, 4];
+        move = mover(items, value);
+    });
+
+    test('can move one space down', function () {
+        const [nextItems, nextValue] = move(0, 1);
+        expect(nextValue).toMatchObject([2, 1, 3, 4]);
+        expect(nextItems).toMatchObject(['2', '1', '3', '4']);
+    });
+
+    test('can move two spaces down', function () {
+        const [nextItems, nextValue] = move(0, 2);
+        expect(nextValue).toMatchObject([2, 3, 1, 4]);
+        expect(nextItems).toMatchObject(['2', '3', '1', '4']);
+    });
+
+    test('can move three spaces down', function () {
+        const [nextItems, nextValue] = move(0, 3);
+        expect(nextValue).toMatchObject([2, 3, 4, 1]);
+
+        const [one, two, three, four] = items;
+        expect(nextItems).toMatchObject(['2', '3', '4', '1']);
+    });
+});
 
 describe('items container', function () {
     let schema;
@@ -47,6 +87,30 @@ describe('items container', function () {
             fireEvent.click(button);
             expect(model.length).toBe(1);
         });
+    });
+
+    test('cannot be mutated if disabled', async function () {
+        let onChange = jest.fn((event, nextModel) => (model = nextModel));
+        let schema = {
+            type: 'array',
+            items: { type: 'number' },
+            readonly: true,
+        };
+        let model = [1];
+        let { container, rerender } = render(
+            h(SchemaForm, { schema, form, model, onChange })
+        );
+
+        const add = container.querySelector('button.add');
+
+        await fireEvent.click(add);
+        await rerender(h(SchemaForm, { schema, model, form, onChange }));
+
+        expect(container.querySelectorAll('ul li').length).toBe(1);
+
+        // 4 because move up, move down, destroy, and add
+        // as long as we only have 1 item in the model we're good
+        expect(container.querySelectorAll('button[disabled]').length).toBe(4);
     });
 });
 
@@ -174,14 +238,17 @@ describe('each item', function () {
                 h(SchemaForm, { schema, form, model, onChange })
             );
 
-            let button = container.querySelectorAll('.array .item .move-up');
-            expect(button).not.toBeNull();
-            expect(button.length).toBe(2);
+            let buttons = container.querySelectorAll('.array .item .move-up');
+            expect(buttons).not.toBeNull();
+            expect(buttons.length).toBe(2);
 
             // Select the second button to move the second element UP into the first slot
-            button = button[1];
-            fireEvent.click(button);
+            let [button1, button2] = buttons;
+            fireEvent.click(button1);
+            expect(onChange).not.toHaveBeenCalled();
+            expect(model).toMatchObject([1, 2]);
 
+            fireEvent.click(button2);
             expect(onChange).toHaveBeenCalled();
             expect(model).toMatchObject([2, 1]);
         });
@@ -192,14 +259,17 @@ describe('each item', function () {
                 h(SchemaForm, { schema, form, model, onChange })
             );
 
-            let button = container.querySelectorAll('.array .item .move-down');
-            expect(button).not.toBeNull();
-            expect(button.length).toBe(2);
+            let buttons = container.querySelectorAll('.array .item .move-down');
+            expect(buttons).not.toBeNull();
+            expect(buttons.length).toBe(2);
 
             // Select the first button to move the first element DOWN into the second slot
-            button = button[0];
-            fireEvent.click(button);
+            let [button1, button2] = buttons;
+            fireEvent.click(button2);
+            expect(onChange).not.toHaveBeenCalled();
+            expect(model).toMatchObject([1, 2]);
 
+            fireEvent.click(button1);
             expect(onChange).toHaveBeenCalled();
             expect(model).toMatchObject([2, 1]);
         });
@@ -220,6 +290,30 @@ describe('each item', function () {
 
             expect(onChange).toHaveBeenCalled();
             expect(model).toMatchObject([2]);
+        });
+    });
+    describe('can specify titleFun', function () {
+        test('which overrides form.title in children', function () {
+            model = [1, 2];
+            form = [
+                {
+                    key: [],
+                    type: 'array',
+                    titleFun(value) {
+                        return `test ${value}`;
+                    },
+                    items: ['[]'],
+                },
+            ];
+
+            const { container } = render(
+                h(SchemaForm, { form, model, schema })
+            );
+
+            expect(container.querySelector('ul li h6')).not.toBeNull();
+            expect(container.querySelector('ul li h6').textContent).toBe(
+                'test 1'
+            );
         });
     });
 });
