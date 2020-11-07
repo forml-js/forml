1ffee603b35aec2086a72b824f1c06ffe9b1188d
Zachary Hueras <zhueras@amdtelemedicine.com>
2020-11-07 05:07:41 -0500

    Update documentation, fix bug

--- src/components/decorator/mui/fieldset.js
+++ src/components/decorator/mui/fieldset.js
@@ -1,5 +1,6 @@
 import FormControl from '@material-ui/core/FormControl';
 import FormLabel from '@material-ui/core/FormLabel';
+import Paper from '@material-ui/core/Paper';
 import { makeStyles } from '@material-ui/core/styles';
 import { createElement as h } from 'react';
 
@@ -8,22 +9,18 @@ import { createElement as h } from 'react';
  */
 const useStyles = makeStyles(function (theme) {
     return {
-        root: { margin: theme.spacing(0, 0, 1, 0) },
+        root: { margin: theme.spacing(0, 0, 1, 0), padding: theme.spacing(1) },
     };
 });
 export default function FieldSet(props) {
     const { title } = props;
     const classes = useStyles();
     return h(
-        Card,
+        Paper,
         { className: classes.root },
-        h(
-            CardContent,
-            {},
-            h(FormControl, { fullWidth: true, component: 'fieldset' }, [
-                h(FormLabel, { key: 'label', component: 'legend' }, title),
-                props.children,
-            ])
-        )
+        h(FormControl, { fullWidth: true, component: 'fieldset' }, [
+            h(FormLabel, { key: 'label', component: 'legend' }, title),
+            props.children,
+        ])
     );
 }
