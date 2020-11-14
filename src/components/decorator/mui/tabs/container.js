eb73b7b746fab93cb5f62166a847982d1c89e086
Zachary Hueras <zhueras@amdtelemedicine.com>
2020-11-13 21:21:24 -0500

    Clean up tabs a bit

--- src/components/decorator/mui/tabs/container.js
+++ src/components/decorator/mui/tabs/container.js
@@ -1,14 +1,32 @@
 import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
-import {createElement as h} from 'react';
+import { createElement as h } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    content: {
+        display: 'flex',
+        flexDirection: 'row',
+        overflow: 'hidden',
+    },
+}));
 
 /**
  * @component
  */
 export default function Container(props) {
-    const {value} = props;
+    const { value } = props;
+    const classes = useStyles(props);
     return h('div', {}, [
-        h(AppBar, {key: 'tab-bar', position: 'static'}, h(Tabs, {value}, props.tabs)),
-        h('div', {key: 'tab-panel'}, props.panels),
+        h(
+            AppBar,
+            { key: 'tab-bar', position: 'static' },
+            h(Tabs, { value }, props.tabs)
+        ),
+        h(
+            'div',
+            { key: 'tab-panel', className: classes.content },
+            props.panels
+        ),
     ]);
 }
--- src/components/decorator/mui/tabs/panel.js
+++ src/components/decorator/mui/tabs/panel.js
@@ -1,13 +1,31 @@
 import { createElement as h } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import clsx from 'clsx';
 
 /**
  * @component
  */
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+        transition: 'all 0.3s',
+        flex: '0 1 0',
+        overflow: 'hidden',
+    },
+    active: {
+        flex: '1',
+    },
+}));
 export default function Panel(props) {
     const { active } = props;
+    const classes = useStyles(props);
 
-    if (!active)
-        return null;
-
-    return h('div', props);
+    return h(Paper, {
+        ...props,
+        className: clsx(props.className, classes.root, {
+            [classes.active]: active,
+        }),
+    });
 }
