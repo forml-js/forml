8e4af3ec2b054c348f1a18f084dbe09f0443675e
Zachary Hueras <zhueras@amdtelemedicine.com>
2022-03-17 07:20:29 -0400

    decorator-mui/arrays: add some padding to item forms

--- packages/decorator-mui/src/arrays/item.jsx
+++ packages/decorator-mui/src/arrays/item.jsx
@@ -17,6 +17,7 @@ const useStyles = makeStyles(function (theme) {
             borderRightWidth: '1px',
             borderRightStyle: 'solid',
             borderRightColor: theme.palette?.divider,
+            padding: theme.spacing(1),
         },
         controls: {
             display: 'flex',
