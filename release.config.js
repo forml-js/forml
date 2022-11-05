8696ee25e786cea1c8ca0e81d175bbf0e37e2c02
Zachary Hueras <zhueras@amdtelemedicine.com>
2022-11-05 01:09:25 -0400

    chore: fix github workflows up

---
+++ release.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    branches: ['main'],
+    plugins: [
+        '@semantic-release/commit-analyzer',
+        ['semantic-release-lerna', { generateNotes: true }],
+        '@semantic-release/changelog',
+        [
+            '@semantic-release/git',
+            {
+                assets: [
+                    'CHANGELOG.md',
+                    'lerna.json',
+                    'package.json',
+                    'package-lock.json',
+                    'packages/*/package.json',
+                ],
+            },
+        ],
+    ],
+};
