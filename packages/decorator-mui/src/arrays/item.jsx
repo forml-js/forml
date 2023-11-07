f58428ea7247ee9618f3c2ce2bb58e3907267769
Zachary Hueras <zhueras@amdtelemedicine.com>
2023-11-07 02:54:56 -0500

    chore(decorator-mui): updated style usage
    Moving away from JSS (makeStyles) as it is deprecated by mui

--- packages/decorator-mui/src/arrays/item.jsx
+++ packages/decorator-mui/src/arrays/item.jsx
@@ -1,144 +1,146 @@
 import Button from '@mui/material/Button';
 import Icon from '@mui/material/Icon';
 import ListItem from '@mui/material/ListItem';
-import makeStyles from '@mui/styles/makeStyles';
-import clsx from 'clsx';
-import React, { forwardRef } from 'react';
+import { styled } from '@mui/material/styles';
+import React, { forwardRef, useMemo } from 'react';
 
-const useStyles = makeStyles(function (theme) {
-    return {
-        forms: {
-            display: 'flex',
-            flexDirection: 'column',
-            flexGrow: '1',
-            borderLeftWidth: '1px',
-            borderLeftStyle: 'solid',
-            borderLeftColor: theme.palette?.divider,
-            borderRightWidth: '1px',
-            borderRightStyle: 'solid',
-            borderRightColor: theme.palette?.divider,
-            padding: theme.spacing?.(1),
-        },
-        spacer: {
-            flex: '1 0 auto',
-            width: 'fill-available',
-            borderBottom: `1px solid ${theme.palette?.divider}`,
-        },
-        controls: {
-            display: 'flex',
-            flexDirection: 'column',
-            '&:has(button:only-child)': {
+const DragHandle = styled('div')(({ theme }) => ({
+    display: 'flex',
+    flexDirection: 'column',
+    padding: theme.spacing(1.5),
+}));
+const FormsContainer = styled('div')(({ theme }) => ({
+    display: 'flex',
+    flexDirection: 'column',
+    flexGrow: '1',
+    borderLeftWidth: '1px',
+    borderLeftStyle: 'solid',
+    borderLeftColor: theme.palette.divider,
+    borderRightWidth: '1px',
+    borderRightStyle: 'solid',
+    borderRightColor: theme.palette.divider,
+    padding: theme.spacing(1),
+}));
+const Controls = styled('div')(({ theme }) => ({
+    display: 'flex',
+    flexDirection: 'column',
+    '&:has(button:only-child)': {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+    },
+}));
+const Spacer = styled('div')(({ theme }) => ({
+    flex: '1 0 auto',
+    width: 'fill-available',
+    borderBottom: `1px solid ${theme.palette?.divider}`,
+}));
+const OnlyDestroy = styled(Button)(({ theme }) => ({
+    display: 'inline-flex !important',
+    flexDirection: 'column',
+    width: theme.spacing?.(6),
+    minWidth: theme.spacing?.(6),
+    border: 0,
+    justifyContent: 'center',
+    alignItems: 'center',
+}));
+const Destroy = styled(Button)(({ theme }) => ({
+    width: theme.spacing?.(6),
+    minWidth: theme.spacing?.(6),
+}));
+const MovementButton = styled(Button)(({ theme, spacer }) => ({
+    borderRadius: '0',
+    width: theme.spacing(6),
+    minWidth: theme.spacing(6),
+    borderBottom: spacer ? 'none !important' : undefined,
+}));
+const StyledListItem = forwardRef(function StyledListItem(props, ref) {
+    return (
+        <ListItem
+            {...props}
+            ref={ref}
+            sx={{
                 display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'center',
-            },
-            '& button:not($destroy):not($onlyDestroy)': {
-                borderRadius: '0',
-                width: theme.spacing?.(6),
-                minWidth: theme.spacing?.(6),
-                '&:has(+ $(spacer))': {
-                    borderBottom: 'none !important',
-                },
-            },
-        },
-        item: {
-            display: 'flex',
-            alignItems: 'stretch',
-            flexDirection: 'row',
-            paddingTop: 0,
-            paddingBottom: 0,
-            backgroundColor: theme.palette?.background?.paper,
-        },
-        icon: {
-            padding: theme.spacing?.(1.5),
-        },
-        dragHandle: {
-            display: 'flex',
-            flexDirection: 'column',
-            padding: theme.spacing?.(1.5),
-        },
-        topDragHandle: {
-            padding: theme.spacing?.(1.5),
-        },
-        onlyDestroy: {
-            display: 'inline-flex !important',
-            flexDirection: 'column',
-            width: theme.spacing?.(6),
-            minWidth: theme.spacing?.(6),
-            border: 0,
-            justifyContent: 'center',
-            alignItems: 'center',
-        },
-        destroy: {
-            width: theme.spacing?.(6),
-            minWidth: theme.spacing?.(6),
-        },
-    };
+                alignItems: 'stretch',
+                flexDirection: 'row',
+                paddingTop: 0,
+                paddingBottom: 0,
+                backgroundColor: 'background.paper',
+            }}
+        />
+    );
 });
+
 /**
  * @component
  * @return {React.Component}
  */
 export function ItemComponent(props, ref) {
-    const classes = useStyles(props);
     const { disabled, form } = props;
     const { draggableProps, dragHandleProps } = props.otherProps;
 
-    const renderMovementButtons =
-        'movementButtons' in form ? form.movementButtons : true;
-    const dragHandleClass = clsx(
-        dragHandleProps ? dragHandleProps.className : null,
-        renderMovementButtons ? classes.topDragHandle : classes.dragHandle
+    const renderMovementButtons = useMemo(
+        () => ('movementButtons' in form ? form.movementButtons : true),
+        [form]
+    );
+    const DestroyButton = useMemo(
+        () => (renderMovementButtons ? Destroy : OnlyDestroy),
+        [renderMovementButtons]
+    );
+
+    console.log(
+        'ItemComponent(dragHandleProps: %o, draggableProps: %o)',
+        dragHandleProps,
+        draggableProps
     );
 
     return (
-        <ListItem
+        <StyledListItem
             divider={true}
-            className={classes.item}
             ref={ref}
             dense
             disableGutters={true}
             {...draggableProps}
         >
-            <div {...dragHandleProps} className={dragHandleClass}>
+            <DragHandle {...dragHandleProps}>
                 <Icon>drag_handle</Icon>
-            </div>
-            <div className={classes.forms}>{props.children}</div>
-            <div key="controls" className={classes.controls}>
-                {renderMovementButtons ? (
-                    <>
-                        <Button
-                            onClick={props.moveUp}
-                            size="small"
-                            disabled={disabled}
-                        >
-                            <Icon>keyboard_arrow_up</Icon>
-                        </Button>
-                        <Button
-                            onClick={props.moveDown}
-                            size="small"
-                            disabled={disabled}
-                        >
-                            <Icon>keyboard_arrow_down</Icon>
-                        </Button>
-                        <div className={classes.spacer} />
-                    </>
-                ) : null}
-                <Button
+            </DragHandle>
+            <FormsContainer>{props.children}</FormsContainer>
+            <Controls key="controls">
+                {useMemo(
+                    () =>
+                        renderMovementButtons ? (
+                            <>
+                                <MovementButton
+                                    onClick={props.moveUp}
+                                    size="small"
+                                    disabled={disabled}
+                                >
+                                    <Icon>keyboard_arrow_up</Icon>
+                                </MovementButton>
+                                <MovementButton
+                                    onClick={props.moveDown}
+                                    size="small"
+                                    disabled={disabled}
+                                    spacer
+                                >
+                                    <Icon>keyboard_arrow_down</Icon>
+                                </MovementButton>
+                                <Spacer />
+                            </>
+                        ) : null,
+                    [renderMovementButtons]
+                )}
+                <DestroyButton
                     onClick={props.destroy}
-                    className={
-                        renderMovementButtons
-                            ? classes.destroy
-                            : classes.onlyDestroy
-                    }
                     color="secondary"
                     size="small"
                     disabled={disabled}
                 >
                     <Icon>delete_forever</Icon>
-                </Button>
-            </div>
-        </ListItem>
+                </DestroyButton>
+            </Controls>
+        </StyledListItem>
     );
 }
 
--- packages/decorator-mui/src/arrays/items.jsx
+++ packages/decorator-mui/src/arrays/items.jsx
@@ -1,4 +1,3 @@
-import clsx from 'clsx';
 import Button from '@mui/material/Button';
 import Icon from '@mui/material/Icon';
 import List from '@mui/material/List';
@@ -6,34 +5,38 @@ import ListItem from '@mui/material/ListItem';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import Paper from '@mui/material/Paper';
-import makeStyles from '@mui/styles/makeStyles';
-import React, { forwardRef } from 'react';
+import React, { useMemo, forwardRef } from 'react';
 import { useLocalizer } from '@forml/hooks';
+import { styled } from '@mui/material/styles';
 
-const useStyles = makeStyles(function (theme) {
-    return {
-        root: {
-            margin: theme.spacing?.(1),
-            flex: '1 1 auto',
-        },
-        disablePadding: {
-            marginTop: 0,
-            marginBottom: 0,
-        },
-        disableGutters: {
-            marginLeft: 0,
-            marginRight: 0,
-        },
-        list: {
+const StyledPaper = styled(Paper)(({ theme, ...props }) => ({
+    margin: theme.spacing?.(1),
+    flex: '1 1 auto',
+    ...(props.disablePadding
+        ? {
+              marginTop: 0,
+              marginBottom: 0,
+          }
+        : {}),
+    ...(props.disableGutters
+        ? {
+              marginLeft: 0,
+              marginRight: 0,
+          }
+        : {}),
+}));
+const StyledList = forwardRef((props, ref) => (
+    <List
+        {...props}
+        ref={ref}
+        htmlName="items"
+        sx={{
             display: 'flex',
             flexDirection: 'column',
             flex: '1 1 auto',
-        },
-        controls: {
-            justifyContent: 'flex-end',
-        },
-    };
-});
+        }}
+    />
+));
 
 /**
  * @component
@@ -43,45 +46,55 @@ function Items(props, ref) {
     const { error, description } = props;
     const { value, disabled } = props;
 
-    const classes = useStyles(props);
-    const color = error ? 'error' : 'info';
     const localizer = useLocalizer();
+    const color = useMemo(() => (error ? 'error' : 'info'), [error]);
 
-    const disablePadding =
-        'disablePadding' in form ? form.disablePadding : false;
-    const disableGutters =
-        'disableGutters' in form ? form.disableGutters : false;
-    const icon = 'icon' in form ? form.icon : 'view_list';
-    const addText =
-        'addText' in form
-            ? localizer.getLocalizedString(form.addText)
-            : title
-            ? `${localizer.getLocalizedString('Add')} ${title}`
-            : localizer.getLocalizedString('Add');
+    const paperProps = useMemo(
+        () => ({
+            disablePadding:
+                'disablePadding' in form ? form.disablePadding : false,
+            disableGutters:
+                'disableGutters' in form ? form.disableGutters : false,
+        }),
+        [form]
+    );
+    const icon = useMemo(
+        () => (
+            <ListItemIcon key="icon" edge="start">
+                <Icon color={color}>
+                    {'icon' in form ? form.icon : 'view_list'}
+                </Icon>
+            </ListItemIcon>
+        ),
+        [form]
+    );
+    const addText = useMemo(
+        () =>
+            'addText' in form
+                ? localizer.getLocalizedString(form.addText)
+                : title
+                ? `${localizer.getLocalizedString('Add')} ${title}`
+                : localizer.getLocalizedString('Add'),
+        [form, localizer, title]
+    );
+    const suffix = useMemo(
+        () =>
+            value && value.length === 0 ? (
+                <ListItem key="empty" divider>
+                    <ListItemText
+                        secondary={localizer.getLocalizedString('empty')}
+                        secondaryTypographyProps={{ align: 'center' }}
+                    />
+                </ListItem>
+            ) : null,
+        [value, localizer]
+    );
 
     return (
-        <Paper
-            className={clsx(classes.root, {
-                [classes.disablePadding]: disablePadding,
-                [classes.disableGutters]: disableGutters,
-            })}
-        >
-            <List
-                disablePadding
-                dense
-                ref={ref}
-                {...otherProps}
-                className={clsx(
-                    otherProps && otherProps.className,
-                    classes.list
-                )}
-            >
+        <StyledPaper {...paperProps}>
+            <StyledList disablePadding dense ref={ref} {...otherProps}>
                 <ListItem key="title" dense={false} divider>
-                    {icon && (
-                        <ListItemIcon key="icon" edge="start">
-                            <Icon color={color}>{icon}</Icon>
-                        </ListItemIcon>
-                    )}
+                    {icon}
                     <ListItemText
                         key="text"
                         primaryTypographyProps={{ color, variant: 'subtitle2' }}
@@ -99,16 +112,9 @@ function Items(props, ref) {
                     </Button>
                 </ListItem>
                 {props.children}
-                {value && value.length === 0 && (
-                    <ListItem key="empty" divider>
-                        <ListItemText
-                            secondary={localizer.getLocalizedString('empty')}
-                            secondaryTypographyProps={{ align: 'center' }}
-                        />
-                    </ListItem>
-                )}
-            </List>
-        </Paper>
+                {suffix}
+            </StyledList>
+        </StyledPaper>
     );
 }
 export default forwardRef(Items);
