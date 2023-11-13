e5638b4f52049a0a9253280c022f49d6e8fc95c8
Zachary Hueras <zhueras@amdtelemedicine.com>
2023-11-13 00:06:56 -0500

    refactor(core): substantial array mapper updates
    Reworked the calculation of array entries and their fields. We now
    calculate child forms based on what's in the model instead of building
    and tracking them separately. Should reduce the computational footprint
    for each render of both the array and its items.

--- packages/core/src/components/mapper/array.jsx
+++ packages/core/src/components/mapper/array.jsx
@@ -1,208 +1,20 @@
 import objectPath from 'objectpath';
 import t from 'prop-types';
-import React, {
-    useEffect,
-    useMemo,
-    useState,
-    forwardRef,
-    useCallback,
-} from 'react';
+import React, { forwardRef, useCallback, useMemo } from 'react';
 import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
 import shortid from 'shortid';
 
+import { useDecorator, useKey, useLocalizer } from '@forml/hooks';
 import { ARRAY_PLACEHOLDER } from '../../constants';
-import { useDecorator, useLocalizer, useModel } from '@forml/hooks';
 import { FormType } from '../../types';
-import {
-    clone,
-    defaultForSchema,
-    getNextSchema,
-    traverseForm,
-} from '../../util';
+import { clone, traverseForm } from '../../util';
 import { SchemaField } from '../schema-field';
 
-function useCallbackGenerator(generator, args) {
-    return useCallback(generator(...args), args);
-}
-
-export function mover(items, value) {
-    return function move(start, end) {
-        return [reorder(items, start, end), reorder(value, start, end)];
-        function reorder(list, start, end) {
-            const result = Array.from(list);
-            const [removed] = result.splice(start, 1);
-            result.splice(end, 0, removed);
-            return result;
-        }
-    };
-}
-
-function _mover(items, value) {
-    return useCallbackGenerator(mover, [items, value]);
-}
-
-export function creator(form) {
-    return function create() {
-        const forms = form.items.map(({ ...form }) => {
-            const key = shortid();
-            return { form, key };
-        });
-
-        const key = shortid();
-        return { forms, key };
-    };
-}
-
-function _creator(form) {
-    return useCallbackGenerator(creator, [form]);
-}
-
-function makeUpwardMover(form, model, move, setItems) {
-    return function upwardMover(index) {
-        function mover(event) {
-            if (index > 0) {
-                const [nextItems, nextValue] = move(index, index - 1);
-                const nextModel = model.setValue(form.key, nextValue);
-                model.onChange(event, nextModel);
-                setItems(nextItems);
-            }
-        }
-
-        return mover;
-    };
-}
-
-function makeDownwardMover(form, model, move, items, setItems) {
-    return function downwardMover(index) {
-        function mover(event) {
-            if (index < items.length - 1) {
-                const [nextItems, nextValue] = move(index, index + 1);
-                const nextModel = model.setValue(form.key, nextValue);
-                model.onChange(event, nextModel);
-                setItems(nextItems);
-            }
-        }
-
-        return mover;
-    };
-}
-
-export function useArrayItems(form, disabled = false) {
-    const [items, setItems] = useState([]);
-    const model = useModel();
-
-    const value = model.getValue(form.key);
-    const move = _mover(items, value);
-    const create = _creator(form);
-
-    useEffect(
-        function () {
-            const nextItems = items ? [...items] : [];
-
-            for (let i = 0; i < value.length; ++i) {
-                if (nextItems[i]) {
-                    nextItems[i].forms = nextItems[i].forms.map(
-                        ({ form: subForm, key }, index) => {
-                            subForm = { ...form.items[index] };
-                            return { form: subForm, key };
-                        }
-                    );
-                } else {
-                    const item = create(i);
-                    nextItems.push(item);
-                }
-            }
-
-            setItems(nextItems);
-        },
-        [form]
-    );
-
-    const add = useCallback(
-        function add(event) {
-            const nextSchema = getNextSchema(form.schema, value.length);
-            const nextModel = model.setValue(
-                [...form.key, value.length],
-                defaultForSchema(nextSchema)
-            );
-            model.onChange(event, nextModel);
-            setItems([...items, create(items.length)]);
-        },
-        [form, value, model, items, create]
-    );
-
-    const destroyer = useCallback(
-        function destroyer(index) {
-            function destroy(event) {
-                const nextValue = Array.from(value);
-                nextValue.splice(index, 1);
-
-                const nextItems = Array.from(items);
-                nextItems.splice(index, 1);
-
-                const nextModel = model.setValue(form.key, nextValue);
-                model.onChange(event, nextModel);
-                setItems(nextItems);
-            }
-
-            return destroy;
-        },
-        [value, items, model, form]
-    );
-
-    const upwardMover = useCallbackGenerator(makeUpwardMover, [
-        form,
-        model,
-        move,
-        setItems,
-    ]);
-    const downwardMover = useCallbackGenerator(makeDownwardMover, [
-        form,
-        model,
-        move,
-        items,
-        setItems,
-    ]);
-
-    return useMemo(() => {
-        if (!disabled) {
-            return {
-                items,
-                setItems,
-                add,
-                destroyer,
-                upwardMover,
-                downwardMover,
-                move,
-            };
-        } else {
-            return {
-                items,
-                setItems,
-                add: /* istanbul ignore next */ () => null,
-                destroyer: () => null,
-                upwardMover: () => null,
-                downwardMover: () => null,
-                move: /* istanbul ignore next */ () => null,
-            };
-        }
-    }, [
-        items,
-        setItems,
-        add,
-        destroyer,
-        upwardMover,
-        downwardMover,
-        move,
-        disabled,
-    ]);
-}
-
 function BaseArrayItem(props, ref) {
-    const { form } = props;
+    const { form, forms, index, disabled } = props;
     const dragDrop = useMemo(
         () => ('dragDrop' in form ? form.dragDrop : true),
-        [form]
+        [parent]
     );
 
     const Component = useMemo(() => {
@@ -213,11 +25,44 @@ function BaseArrayItem(props, ref) {
         }
     }, [dragDrop]);
 
-    return <Component {...props} ref={ref} />;
+    const fields = useMemo(
+        () =>
+            forms.map(function (formTemplate) {
+                if (!formTemplate) return;
+                const form = copyWithIndex(formTemplate, index);
+                const path = objectPath.stringify(form.key);
+
+                /**
+                 * Override properties of the child form
+                 * titleFun - to generate a title for the sub-form
+                 * disabled - to propagate the disabled state to children
+                 */
+                form.titleFun =
+                    'titleFun' in form ? form.titleFun : parent.titleFun;
+                form.readonly = 'readonly' in form ? form.readonly : disabled;
+
+                return (
+                    <SchemaField
+                        key={path}
+                        form={form}
+                        schema={form.schema}
+                        parent={parent}
+                    />
+                );
+            }),
+        [parent, forms, index]
+    );
+
+    return (
+        <Component {...props} ref={ref}>
+            {fields}
+        </Component>
+    );
 }
 
 const NormalArrayItem = forwardRef(function NormalArrayItem(props, ref) {
-    const { form, items, value, index } = props;
+    const { form, array, value, index, dragHandleProps, draggableProps } =
+        props;
     const { readonly: disabled } = form;
     const deco = useDecorator();
     const localizer = useLocalizer();
@@ -233,11 +78,11 @@ const NormalArrayItem = forwardRef(function NormalArrayItem(props, ref) {
 
     const actions = useMemo(
         () => ({
-            destroy: items.destroyer(index),
-            moveUp: items.upwardMover(index),
-            moveDown: items.downwardMover(index),
+            destroy: () => array.removeArray(index),
+            moveUp: () => array.moveArrayUp(index),
+            moveDown: () => array.moveArrayDown(index),
         }),
-        [items, index]
+        [array.removeArray, array.moveArrayUp, array.moveArrayDown, index]
     );
 
     return (
@@ -246,8 +91,9 @@ const NormalArrayItem = forwardRef(function NormalArrayItem(props, ref) {
             title={title}
             index={index}
             form={form}
+            dragHandleProps={dragHandleProps}
+            draggableProps={draggableProps}
             {...actions}
-            {...props}
             ref={ref}
         >
             {props.children}
@@ -257,22 +103,12 @@ const NormalArrayItem = forwardRef(function NormalArrayItem(props, ref) {
 
 function DraggableItemFactory(props, ref) {
     return function DraggableItem(provided) {
-        const { draggableProps, dragHandleProps } = provided;
         const injectRef = (e) => {
             provided.innerRef(e);
             if (ref) ref(e);
         };
-        const otherProps = useMemo(
-            () => ({ ...props.otherProps, draggableProps, dragHandleProps }),
-            [props, draggableProps, dragHandleProps]
-        );
         return (
-            <NormalArrayItem
-                {...props}
-                {...provided}
-                otherProps={otherProps}
-                ref={injectRef}
-            >
+            <NormalArrayItem {...props} {...provided} ref={injectRef}>
                 {props.children}
             </NormalArrayItem>
         );
@@ -305,63 +141,38 @@ export const ArrayItem = forwardRef(BaseArrayItem);
  * <ArrayComponent value={value} form={form} error={error} />
  * ```
  */
+
 function ArrayComponent(props, ref) {
-    const { form, value } = props;
+    const { form, schema, value } = props;
     const { readonly: disabled } = form;
 
-    const items = useArrayItems(form, disabled);
     const type = useMemo(() => objectPath.stringify(form.key), [form.key]);
+    const array = useKey(form.key);
 
     const parent = form;
-    const arrays = useMemo(
+    const items = useMemo(
         function () {
             const arrays = [];
-            for (let i = 0; i < items.items.length; ++i) {
-                const item = items.items[i];
-                const forms = item.forms.map(function ({ form, key }) {
-                    if (!form) return;
-                    const formCopy = copyWithIndex(form, i);
-
-                    /**
-                     * Override properties of the child form
-                     * titleFun - to generate a title for the sub-form
-                     * disabled - to propagate the disabled state to children
-                     */
-                    formCopy.titleFun =
-                        'titleFun' in formCopy
-                            ? formCopy.titleFun
-                            : form.titleFun;
-                    formCopy.readonly =
-                        'readonly' in formCopy ? formCopy.readonly : disabled;
-
-                    return (
-                        <SchemaField
-                            key={key}
-                            form={formCopy}
-                            schema={formCopy.schema}
-                            parent={parent}
-                        />
-                    );
-                });
-
+            const count = array.model?.length ?? 0;
+            for (let index = 0; index < count; ++index) {
+                const key = [...parent.key, index];
+                const path = objectPath.stringify(key);
                 arrays.push(
                     <ArrayItem
-                        key={item.key}
-                        id={item.key}
+                        key={path}
+                        array={array}
+                        id={path}
                         form={form}
-                        index={i}
-                        value={value[i]}
-                        items={items}
-                        item={item}
+                        forms={parent.items}
+                        index={index}
                         type={type}
-                    >
-                        {forms}
-                    </ArrayItem>
+                        schema={schema}
+                    />
                 );
             }
             return arrays;
         },
-        [items.items]
+        [type, form, array]
     );
 
     const dragDrop = useMemo(
@@ -373,17 +184,16 @@ function ArrayComponent(props, ref) {
     );
 
     return (
-        <Component items={items} {...props}>
-            {arrays}
+        <Component array={array} {...props}>
+            {items}
         </Component>
     );
 }
 
 const DraggableArrayContainer = forwardRef(
     function DraggableArrayContainer(props, ref) {
-        const { form, items } = props;
+        const { form, array } = props;
         const droppableId = useMemo(shortid);
-        const model = useModel();
         const onDragEnd = useCallback(
             function onDragEnd(result) {
                 if (!result.destination) {
@@ -391,16 +201,13 @@ const DraggableArrayContainer = forwardRef(
                 } else if (result.destination.index === result.source.index) {
                     return;
                 } else {
-                    const [nextItems, nextValue] = items.move(
+                    array.moveArray(
                         result.source.index,
                         result.destination.index
                     );
-                    const nextModel = model.setValue(form.key, nextValue);
-                    model.onChange({ target: { value: nextModel } }, nextModel);
-                    items.setItems(nextItems);
                 }
             },
-            [items, model, form]
+            [array.moveArray]
         );
         const renderDraggableItems = useCallback(
             (provided) => {
@@ -409,7 +216,11 @@ const DraggableArrayContainer = forwardRef(
                     if (ref) ref(e);
                 };
                 return (
-                    <NormalArrayContainer {...props} ref={injectRef}>
+                    <NormalArrayContainer
+                        array={array}
+                        {...props}
+                        ref={injectRef}
+                    >
                         {props.children}
                         {provided.placeholder}
                     </NormalArrayContainer>
@@ -430,30 +241,29 @@ const DraggableArrayContainer = forwardRef(
 
 const NormalArrayContainer = forwardRef(
     function NormalArrayContainer(props, ref) {
-        const { form, items } = props;
+        const { form, array } = props;
         const { readonly: disabled, titleFun } = form;
-        const model = useModel();
-        const value = model.getValue(form.key);
         const deco = useDecorator();
         const localizer = useLocalizer();
         const title = useMemo(
-            () => localizer.getLocalizedString(titleFun?.(value) ?? form.title),
-            [localizer, value, form, titleFun]
+            () =>
+                localizer.getLocalizedString(
+                    titleFun?.(array.model) ?? form.title
+                ),
+            [localizer, array.model, form, titleFun]
         );
         const description = localizer.getLocalizedString(form.description);
         const { error } = props;
-        const otherProps = useMemo(() => form.otherProps ?? {}, [form]);
 
         return (
             <deco.Arrays.Items
                 className={form.htmlClass}
-                add={items.add}
-                value={value}
+                add={array.appendArray}
+                value={array.model}
                 title={title}
                 description={description}
                 error={error}
                 ref={ref}
-                otherProps={otherProps}
                 disabled={disabled}
                 form={form}
             >
