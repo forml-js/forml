4c41c6fa9cd45eab075799dfc3eda62b9db0b63a
Zachary Hueras <zhueras@amdtelemedicine.com>
2023-11-13 00:13:01 -0500

    refactor(core): split SchemaField in two
    Given a key, render a ValueField
    Given no key, render sub-forms in WrapperField
    
    This can help prevent unnecessary rerenders. Since ValueField uses the
    model context, it rerenders every time the model is updated.
    
    WrapperField only uses the renderer context, so rerendering those fields
    when the model context changes doesn't make much sense.

--- packages/core/src/components/schema-field.jsx
+++ packages/core/src/components/schema-field.jsx
@@ -1,55 +1,66 @@
 import debug from 'debug';
-import ObjectPath from 'objectpath';
 import PropTypes from 'prop-types';
 import React, { useMemo, useCallback } from 'react';
 
-import { useMapper, useModel } from '@forml/hooks';
+import { useMappedField, useKey } from '@forml/hooks';
 import { FormType } from '../types';
 
 const log = debug('forml:schema-field');
 
-export function SchemaField(props) {
-    const { schema, form, parent } = props;
+function ValueField(props) {
+    const { form, parent } = props;
 
-    const mapper = useMapper();
-    const model = useModel();
-    const Field = useMemo(() => mapper[form.type], [mapper, form]);
-    const key = useMemo(
-        () => (form.key !== undefined ? ObjectPath.stringify(form.key) : ''),
-        [form]
-    );
-
+    const Field = useMappedField(form.type);
+    const field = useKey(form.key);
     const onChange = useCallback(
-        function onChange(event, value) {
-            const newModel = model.setValue(form.key, value);
-            model.onChange(event, newModel);
+        (event, value) => {
+            field.setValue(value);
         },
-        [model, form]
+        [field.setValue]
     );
-
-    const [value, error] = useMemo(() => {
-        if (Field && form.key) {
-            return [model.getValue(form.key), model.getError(form.key)];
-        } else {
-            return [undefined, undefined];
-        }
-    }, [model, form]);
 
     if (!Field) {
-        log('SchemaField(%s) : !Field : form : %o', key, form);
+        log(
+            'ValueField.fail(key: %o, form: %o) : !Field : form : %o',
+            form.key,
+            form
+        );
         return null;
     }
 
     return (
         <Field
-            schema={schema}
             form={form}
-            value={value}
+            schema={field.schema}
+            value={field.model}
             onChange={onChange}
-            error={error}
+            //error={error}
             parent={parent}
         />
     );
+}
+
+function WrapperField(props) {
+    const { form, parent } = props;
+    const Field = useMappedField(form.type);
+
+    if (!Field) {
+        log('SchemaField.fail(key: %o, form: %o)', form.key, form);
+        return null;
+    }
+
+    return <Field form={form} parent={parent} />;
+}
+
+export function SchemaField(props) {
+    const { form } = props;
+
+    const Component = useMemo(
+        () => ('key' in form ? ValueField : WrapperField),
+        [form.key]
+    );
+
+    return <Component {...props} />;
 }
 
 SchemaField.propTypes = {
