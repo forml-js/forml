35e5966c3c2efecced6be233b47625d41e268ae7
Zachary Hueras <zachary.hueras@gmail.com>
2026-04-04 19:45:57 -0400

    chore(decorator-mantine): use new Icon component in Tab

--- packages/decorator-mantine/src/tabs/tab.jsx
+++ packages/decorator-mantine/src/tabs/tab.jsx
@@ -1,15 +1,12 @@
 import { Tabs } from '@mantine/core';
-import * as Icons from '@tabler/icons-react';
+import Icon from '../icon.jsx';
 import React, { useMemo } from 'react';
 
 export default function Tab(props) {
     const { form, index } = props;
     const icon = useMemo(() => {
-        if (form.icon) {
-            let icon = form.icon.replace(/^([a-z])/, (c) => c.toUpperCase());
-            icon = icon.replace(/_([a-z])/g, (c) => c[1].toUpperCase());
-            icon = `Icon${icon}`;
-            return Icons[icon];
+        if ('icon' in form) {
+            return <Icon icon={form.icon} />;
         } else {
             return undefined;
         }
