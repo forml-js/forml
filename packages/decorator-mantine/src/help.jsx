a0fa460f94f6da81826534e9bda700c0035c6f44
Zachary Hueras <zachary.hueras@gmail.com>
2026-04-04 19:44:22 -0400

    fix(decorator-mantine): help now honors form settings

--- packages/decorator-mantine/src/help.jsx
+++ packages/decorator-mantine/src/help.jsx
@@ -1,8 +1,38 @@
-import React from 'react';
-import { Text } from '@mantine/core';
+import React, { useMemo } from 'react';
+import { Text, Title } from '@mantine/core';
+import { using } from '@forml/hooks';
 
 export default function Help(props) {
     const { form } = props;
     const { description } = form;
-    return <Text size="sm">{description}</Text>;
+
+    const [Component, attributes] = useMemo(() => {
+        const props = using(form);
+        props.add('size').from('size', 'sm');
+        props.add('fw').from('weight');
+        props.add('fs').from('style');
+        props.add('td').from('decoration');
+        props.add('c').from('color');
+        props.add('tt').from('transform');
+        props.add('ta').from('align');
+
+        const variant = 'variant' in form ? form.variant : 'body';
+        let Component = Text;
+        if (variant.startsWith('h')) {
+            Component = Title;
+            props.add('size', form.variant);
+        } else if (variant === 'span') {
+            props.add('component', 'span');
+        }
+
+        return [Component, props.value];
+    }, [form]);
+
+    console.log(
+        'Help(attributes: %o, description: %o)',
+        attributes,
+        description
+    );
+
+    return <Component {...attributes}>{description}</Component>;
 }
